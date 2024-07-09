5f8c2808dbcf4dbce93bf49cbcff0989e3d2c55b
Fahd011 <fahdaleem256@gmail.com>
2024-07-09 16:03:34 +0500

    Added user login and signup api's

---
+++ backend/controllers/userController.js
@@ -0,0 +1,38 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
+
+// login user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    // create a token
+    const token = createToken(user._id);
+    res.status(201).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// signup user
+const signupUser = async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  try {
+    const user = await User.signup(name, email, password, pic);
+
+    // create a token
+    const token = createToken(user._id);
+    res.status(201).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { loginUser, signupUser };
