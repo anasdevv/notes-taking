7cb39250682233a261d622e6a9925ac3d07bc5e3
Fahd011 <fahdaleem256@gmail.com>
2024-07-22 23:57:15 +0500

    Completed user profile page

---
+++ frontend/src/constants/userConstants.jsx
@@ -0,0 +1,8 @@
+const BASE_URL = "http://localhost:5000/api";
+
+export const USER_ROUTES = {
+  SIGN_UP: `${BASE_URL}/users/signup`,
+  LOG_IN: `${BASE_URL}/users/login`,
+  IMAGE_UPLOAD: "https://api.cloudinary.com/v1_1/dq196vyns/image/upload",
+  // Add other routes here as needed
+};
