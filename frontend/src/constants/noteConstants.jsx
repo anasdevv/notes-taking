e4920bcf9ffb032b4a671469dd4a1b693c260259
HJ <anas1.dev@gmail.com>
2024-11-29 15:56:51 +0500

    small changes and dockerize the app

--- frontend/src/constants/noteConstants.jsx
+++ frontend/src/constants/noteConstants.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_BASE_URL ?? 'http://localhost:5000/api';
 
 export const NOTE_ROUTES = {
   GET_ALL_NOTES: `${BASE_URL}/notes/`,
--- frontend/src/constants/userConstants.jsx
+++ frontend/src/constants/userConstants.jsx
@@ -1,9 +1,9 @@
-const BASE_URL = "http://localhost:5000/api";
-
+const BASE_URL = import.meta.env.VITE_BASE_URL ?? 'http://localhost:5000/api';
+console.log('Base URL ', BASE_URL);
 export const USER_ROUTES = {
   SIGN_UP: `${BASE_URL}/users/signup`,
   LOG_IN: `${BASE_URL}/users/login`,
-  IMAGE_UPLOAD: "https://api.cloudinary.com/v1_1/dq196vyns/image/upload",
+  IMAGE_UPLOAD: 'https://api.cloudinary.com/v1_1/dq196vyns/image/upload',
   UPDATE_PROFILE: `${BASE_URL}/users/profile`,
   // Add other routes here as needed
 };
